0dd53d10035ff5d6fb6d76d7b0aa03bed39d93c1
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-06-30 15:23:31 +0530

    added personal Library api backend files

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const connection = require("./config/db");
+
+const dotenv = reuire("dotenv");
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 6060 ;
+
+app.get("/",(req,res){
+    res.send("WELCOME TO PERSONAL LIBRARY")
+})
+
+
+app.listen(port,async()=>{
+    try {
+        await connection;
+        console.log(`Connected to MongoDB and Server is running on port ${port}`)
+    } catch (error) {
+        console.log("Error in connecting to port ")
+    }
+})
